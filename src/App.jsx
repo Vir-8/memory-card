e54448c1f71eebf05269c778e451c7d98fe4e96d
Vir <73329256+Vir-8@users.noreply.github.com>
2023-08-01 10:33:52 +0530

    Generate random images using PokeAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const [usedNumbers, setUsedNumbers] = useState([]);
+	let images = [];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const Image = () => {
+		let pokemonNumber;
+
+		do {
+			pokemonNumber = Math.floor(Math.random() * 151) + 1;
+		} while (usedNumbers.includes(pokemonNumber));
+		setUsedNumbers[{ pokemonNumber }];
+
+		return (
+			<img
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonNumber}.png`}
+				className="logo"
+				alt="Vite logo"
+				onClick={regenerateImages}
+			/>
+		);
+	};
+
+	const GenerateImages = () => {
+		images = [];
+		for (let i = 0; i < 10; i++) {
+			images.push(<Image />);
+		}
+		return images;
+	};
+
+	function regenerateImages() {
+		images = [];
+		setUsedNumbers([]);
+	}
+
+	return (
+		<>
+			<div>{<GenerateImages />}</div>
+		</>
+	);
 }
 
-export default App
+export default App;
