87b067a17d09c6f7fabec2ff707e05f0807b9b2f
Vir <73329256+Vir-8@users.noreply.github.com>
2023-08-03 18:18:44 +0530

    Clean code

--- src/components/genButton.jsx
+++ src/components/genButton.jsx
@@ -1,8 +1,21 @@
 const GenButton = ({ gen, loadGen }) => {
+	const genRanges = {
+		1: { min: 1, max: 151 },
+		2: { min: 152, max: 251 },
+		3: { min: 252, max: 386 },
+		4: { min: 387, max: 493 },
+		5: { min: 494, max: 649 },
+		6: { min: 650, max: 721 },
+		7: { min: 722, max: 809 },
+		8: { min: 810, max: 905 },
+	};
+
+	const { min, max } = genRanges[gen] || { min: 0, max: 0 };
+
 	return (
 		<button
 			onClick={() => {
-				loadGen(gen);
+				loadGen(min, max);
 			}}>
 			Gen {gen}
 		</button>
