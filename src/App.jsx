f80380d34218abe05ec9692e1b070ab5ed853bc3
Vir <73329256+Vir-8@users.noreply.github.com>
2023-08-03 18:18:32 +0530

    Implement start menu, clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import ImageGenerator from "./components/imageGeneration";
+import ImageGenerator from "./scripts/imageGenerator";
 import Score from "./components/score";
 import GenButton from "./components/genButton";
 
@@ -8,8 +8,10 @@ function App() {
 	const [score, setScore] = useState(0);
 	const [bestScore, setBestScore] = useState(0);
 	const [genLoaded, setGenLoaded] = useState(false);
+	const [menuLoaded, setMenuLoaded] = useState(false);
 	const [min, setMin] = useState(0);
 	const [max, setMax] = useState(0);
+	const genValues = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 	const updateScore = (status) => {
 		if (status == "win") {
@@ -23,22 +25,14 @@ function App() {
 		setBestScore(newBestScore);
 	};
 
-	const loadGen = (gen) => {
-		const genRanges = {
-			1: { min: 1, max: 151 },
-			2: { min: 152, max: 251 },
-			3: { min: 252, max: 386 },
-			4: { min: 387, max: 493 },
-			5: { min: 494, max: 649 },
-			6: { min: 650, max: 721 },
-			7: { min: 722, max: 809 },
-			8: { min: 810, max: 905 },
-		};
+	const showMenu = () => {
+		setMenuLoaded(true);
+	};
 
-		const { min, max } = genRanges[gen] || { min: 0, max: 0 };
+	const loadGen = (min, max) => {
 		setMin(min);
 		setMax(max);
-
+		setMenuLoaded(false);
 		setGenLoaded(true);
 	};
 
@@ -58,16 +52,17 @@ function App() {
 						max={max}
 					/>
 				</>
-			) : (
+			) : null}
+			{menuLoaded && (
 				<>
-					<GenButton gen="1" loadGen={loadGen}></GenButton>
-					<GenButton gen="2" loadGen={loadGen}></GenButton>
-					<GenButton gen="3" loadGen={loadGen}></GenButton>
-					<GenButton gen="4" loadGen={loadGen}></GenButton>
-					<GenButton gen="5" loadGen={loadGen}></GenButton>
-					<GenButton gen="6" loadGen={loadGen}></GenButton>
-					<GenButton gen="7" loadGen={loadGen}></GenButton>
-					<GenButton gen="8" loadGen={loadGen}></GenButton>
+					{genValues.map((gen) => (
+						<GenButton key={gen} gen={gen} loadGen={loadGen}></GenButton>
+					))}
+				</>
+			)}
+			{!menuLoaded && !genLoaded && (
+				<>
+					<button onClick={showMenu}>Start!</button>
 				</>
 			)}
 		</>
