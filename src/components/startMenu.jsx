27b9722b41b8eeeabea81ee694acdb910a59447c
Vir <73329256+Vir-8@users.noreply.github.com>
2023-08-07 17:55:27 +0530

    Add title header animation

--- src/components/startMenu.jsx
+++ src/components/startMenu.jsx
@@ -1,9 +1,31 @@
+import React from "react";
+import { useState } from "react";
 import titleImage from "../assets/title.png";
 
 function StartMenu({ onButtonClick }) {
+	const [animationPlayed, setAnimationPlayed] = useState(true);
+
+	const handleMouseEnter = () => {
+		if (!animationPlayed) {
+			setAnimationPlayed(true);
+		}
+	};
+
+	const handleAnimationEnd = () => {
+		setTimeout(() => {
+			setAnimationPlayed(false);
+		}, 400);
+	};
+
 	return (
 		<div className="startMenu">
-			<img className="startMenuHeader" src={titleImage} alt="title" />
+			<img
+				className={`startMenuHeader ${animationPlayed ? "animateHeader" : ""}`}
+				onMouseEnter={handleMouseEnter}
+				onAnimationEnd={handleAnimationEnd}
+				src={titleImage}
+				alt="title"
+			/>
 			<button className="startButton" onClick={onButtonClick}>
 				Start Game
 			</button>
