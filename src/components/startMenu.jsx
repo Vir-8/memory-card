34855b283f317cb46e620ae6dc9da84694117f00
Vir <73329256+Vir-8@users.noreply.github.com>
2023-12-15 14:23:26 +0530

    Update title screen animation

--- src/components/startMenu.jsx
+++ src/components/startMenu.jsx
@@ -1,28 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import titleImage from "../assets/title.png";
 
 function StartMenu({ onButtonClick }) {
-	const [animationPlayed, setAnimationPlayed] = useState(true);
-
-	const handleMouseEnter = () => {
-		if (!animationPlayed) {
-			setAnimationPlayed(true);
-		}
-	};
-
-	const handleAnimationEnd = () => {
-		setTimeout(() => {
-			setAnimationPlayed(false);
-		}, 400);
-	};
-
 	return (
 		<div className="startMenu">
 			<img
-				className={`startMenuHeader ${animationPlayed ? "animateHeader" : ""}`}
-				onMouseEnter={handleMouseEnter}
-				onAnimationEnd={handleAnimationEnd}
+				className={`startMenuHeader animateHeader`}
 				src={titleImage}
 				alt="title"
 			/>
